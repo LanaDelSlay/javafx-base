927078bc558b99dd67f0c860a7ca7c511166baae
Graham <gmiller1902@gmail.com>
2020-12-14 23:40:52 -0800

    Revert "Added loading saved addresses on MacOS"
    This reverts commit d6246291840c9fdb625cb42c33689de249e4cbed.

--- src/main/java/hellofx/Launcher.java
+++ src/main/java/hellofx/Launcher.java
@@ -1,17 +1,9 @@
 package hellofx;
 
-import java.awt.FileDialog;
-
-import javax.swing.JFrame;
-
 public class Launcher {
     
     public static void main(String[] args) {
     	HelloFX.main(args);
 //        SceneBuilderFX.main(args);
     }
-    
- 
-    
-    
 }
